8207d96bceca19d616eaa1b143a4639f73e34c6d
asadchauhan1000 <asadchauhan1000@gmail.com>
2026-02-05 22:40:06 +0530

    feat: MinMax program

---
+++ src/MinMax.java
@@ -0,0 +1,21 @@
+public class MinMax {
+    public static void main(String args[]){
+        int[] a={3,4,2,4,6,-1,4,1,9};
+        int min,max;
+        min=a[0];
+        max=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]<min){
+                min=a[i];
+            }}
+            for(int i=1;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        System.out.println("Min  "+min);
+        System.out.println("Max  "+max);
+
+    }
+    
+}
